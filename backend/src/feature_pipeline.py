d0a7a4bd1a53a2d05cf8caba5edd9fdededdbda4
Aayan Mapara <amapara2007@gmail.com>
2025-11-12 17:05:18 -0700

    refactored feature_pipeline

--- backend/src/feature_pipeline.py
+++ backend/src/feature_pipeline.py
@@ -11,25 +11,34 @@ load_dotenv(env_path)
 
 ETHERSCAN_API_KEY = os.environ.get('ETHERSCAN_API_KEY')
 url = "https://api.etherscan.io/v2/api"
-query= {
-            "apikey": ETHERSCAN_API_KEY,
-            "chainid":"1",
-            "module": "account",
-            "action": "txlist",
-            "address":"0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC",
-            "tag":"latest",
-            "startblock":"0",
-            "endblock":"9999999999",
-            "page":"1",""
-            "offset":"1",
-            "sort":"desc"
-        }
 
-response = requests.get(url, params = query)
-history_dict = response.json()
+def get_address_history(address):
+    query= {
+                "apikey": ETHERSCAN_API_KEY,
+                "chainid":"1",
+                "module": "account",
+                "action": "txlist",
+                "address":"0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC",
+                "tag":"latest",
+                "startblock":"0",
+                "endblock":"9999999999",
+                "page":"1",""
+                "offset":"1",
+                "sort":"desc"
+            }
 
-df = pd.json_normalize(history_dict, 'result')
-df.to_csv('history.csv', index = False)
+    response = requests.get(url, params = query)
+    history_dict = response.json()
+
+    csv_path = os.path.join(os.path.dirname(__file__), '..', 'data/' 'history.csv')
+
+    df = pd.json_normalize(history_dict, 'result')
+    df.to_csv(csv_path, index = False)
+
+    return df
+
+def get_address_features(data):
+    return
 
 
 
